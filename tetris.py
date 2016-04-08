aa958b8d24e8e4040202187e537532e7f39b5dfb
Claire Abu-Assal <spooky121@gmail.com>
2016-04-07 19:46:35 -0800

    Cleaned up tetris.py and changed shape orientation on spawn.

--- tetris.py
+++ tetris.py
@@ -2,6 +2,24 @@ import os, sys
 import pygame
 from pygame.locals import *
 from shape import Shape
+import copy
+
+def collided(shape, blocks): #checks for collision between a shape and a list of blocks
+	collide = -1
+		
+	for b in shape.blocks:  #check for top/bottom collisions
+		collide = max(collide, b.collidelist(blocks))
+	
+	return collide > -1
+		
+
+def is_valid(direction, shape, blocks): #checks a shape against the current blocks to see if movement is legal 
+	if blocks == []:
+		return True
+	if direction == 'right':
+		return True 
+	if direction == 'left':
+		return True
 
 def main():
 	pygame.init()
@@ -19,37 +37,36 @@ def main():
 			
 			elif event.type == KEYDOWN:
 				if event.key == K_RIGHT:
-					current_shape.status = 'moveright'
+					if is_valid('right', current_shape, blocks):
+						current_shape.status = 'moveright'
 				if event.key == K_LEFT:
-					current_shape.status = 'moveleft'
+					if is_valid('left', current_shape, blocks):
+						current_shape.status = 'moveleft'
 			elif event.type == KEYUP:
 				current_shape.status = None
 
-		collide = -1
 		
-		for b in current_shape.blocks:
-			collide = max(collide, b.collidelist(blocks))
-		
-		if collide > -1:
+		if collided(current_shape, blocks):
 			current_shape.at_bottom = True
-
-
-		screen.fill(white)
-		flag = current_shape.update() #flags if shape reached bottom or collided with another block
 		
-		if flag:
+		flag = current_shape.update() #flags if shape reached bottom or top/bottom collided with another block
+
+		if flag: #shape can't go down further so add its blocks to the list of blocks
 			blocks = blocks + current_shape.blocks
 			current_shape = Shape()
+		
+		screen.fill(white)
 
-		for b in current_shape.blocks:
-			pygame.draw.rect(screen, b.color, b, 0)
-			pygame.draw.rect(screen, black, b, 1)
-		for bk in blocks:
+		for b in current_shape.blocks: #draw the current shape's blocks on the screen
+			pygame.draw.rect(screen, b.color, b, 0) #once for fill
+			pygame.draw.rect(screen, black, b, 1)	#once for outline
+		
+		for bk in blocks: #draw all other blocks on the screen
 			pygame.draw.rect(screen, bk.color, bk, 0)
 			pygame.draw.rect(screen, black, bk, 1)
 		
 		pygame.display.update()
-		pygame.time.delay(150)
+		pygame.time.delay(200)
 		
 	
 
